f8a5361c1c4484348152881c3c64f0d686c08e53
Deepak kumar <67201893+Deepakd25k@users.noreply.github.com>
2022-10-27 08:38:57 +0530

    Time: 2 ms (79.23%), Space: 42.2 MB (5.07%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }
+        if (num == 1) {
+            return true;
+        }
+        int[] primes = new int[]{2, 3, 5};
+        for (int i = 0; i < primes.length; i++) {
+            while (num % primes[i] == 0) {
+                num = num / primes[i];
+            }
+        }
+        return num == 1;
+    }
+}
