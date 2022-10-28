50160cd917b62b2dd8fb03321edd7ba68c4298a5
Deepak kumar <67201893+Deepakd25k@users.noreply.github.com>
2022-10-28 08:53:21 +0530

    Time: 8 ms (21.95%), Space: 44.6 MB (68.64%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length, curMax = nums[0], max = nums[0], curMin = nums[0];
+    
+    for(int i=1; i<n; i++){
+		/*
+		nums[i] * curMin -> because if current number is -ve and curMin is also -ve then 
+		mutiply both would be curMax*/
+        
+        int temp = curMax;
+        curMax = Math.max(nums[i], Math.max(nums[i]*curMax, nums[i]*curMin));
+        curMin = Math.min(nums[i], Math.min(nums[i]*temp, nums[i]*curMin));
+        max = Math.max(max, curMax);
+    }
+    
+    return max;
+    }
+}
